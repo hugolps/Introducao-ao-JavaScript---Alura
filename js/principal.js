caee3a05cd623eabb940bab59b9465977c76ef1a
Hugo Leonardo <59965105+hugolps@users.noreply.github.com>
2020-05-13 15:24:28 -0300

    validando os campos e  mensagens de erro

--- js/principal.js
+++ js/principal.js
@@ -15,17 +15,17 @@ for (var i = 0; i < pacientes.length; i++) {
 
     var tdImc = paciente.querySelector(".info-imc");
 
-    var pesoEhValido = true;
-    var alturaEhValida = true;
+    var pesoEhValido = validaPeso(peso);
+    var alturaEhValida = validaAltura(altura);
 
-    if (peso <= 0 || peso >= 1000) {
+    if (!pesoEhValido) {
         console.log("Peso inválido!");
         pesoEhValido = false;
         tdImc.textContent = "Peso inválido";
         paciente.classList.add("paciente-invalido");
     }
 
-    if (altura <= 0 || altura >= 3.00) {
+    if (!alturaEhValida) {
         console.log("Altura inválida!");
         alturaEhValida = false;
         tdImc.textContent = "Altura inválida";
@@ -33,7 +33,33 @@ for (var i = 0; i < pacientes.length; i++) {
     }
 
     if (pesoEhValido && alturaEhValida) {
-        var imc = peso / (altura * altura);
-        tdImc.textContent = imc.toFixed(2);
+        var imc = calculaImc(peso,altura);
+        tdImc.textContent = imc;
     }
+}
+
+function validaPeso(peso) {
+
+    if(peso >= 0 && peso <= 1000){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function validaAltura(altura) {
+
+    if(altura >=0 && altura <= 3.0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function calculaImc(peso,altura) {
+    var imc = 0;
+
+    imc = peso / (altura * altura);
+    console.log(imc);
+    return imc.toFixed(2);
 }
